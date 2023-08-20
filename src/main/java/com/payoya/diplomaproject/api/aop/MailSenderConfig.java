2e9657f8ba59f0b074e72913145d6ee5839bcecf
Alex <asdw3a@gmail.com>
2023-08-20 19:21:31 +0300

    starting working with aop aspectJ in java

--- src/main/java/com/payoya/diplomaproject/api/aop/MailSenderConfig.java
+++ src/main/java/com/payoya/diplomaproject/api/aop/MailSenderConfig.java
@@ -1,6 +1,18 @@
 package com.payoya.diplomaproject.api.aop;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class MailSenderConfig {
+
+    @AfterReturning("execution(* com.payoya.diplomaproject.api.service.UserService.createNewUser())")
+    public void afterUserIsCreate(){
+        System.err.println("user is created");
+    }
 
     //TODO : create an AOP classes for sending emails by email addresses after successful registration
 
