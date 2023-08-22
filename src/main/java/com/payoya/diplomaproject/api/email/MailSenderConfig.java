d3d703bae8e31e8aea413fe91c11fb1d866b4097
Alex <asdw3a@gmail.com>
2023-08-22 19:09:08 +0300

    implemented sending emails in async way

--- src/main/java/com/payoya/diplomaproject/api/email/MailSenderConfig.java
+++ src/main/java/com/payoya/diplomaproject/api/email/MailSenderConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.time.LocalDateTime;
 import java.util.Properties;
 
 @Configuration
+@EnableAsync
 public class MailSenderConfig {
 
     //TODO create an MailConfig for sending mails for created users
@@ -28,10 +30,10 @@ public class MailSenderConfig {
         //spring.mail.password=lsqsnvqclemvpxqt
 
         Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
+//        props.put("mail.transport.protocol", "smtp"); // for debug purposes
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+//        props.put("mail.debug", "true");              // for debug purposes
 
         return mailSender;
     }
--- src/main/java/com/payoya/diplomaproject/api/email/MailSenderService.java
+++ src/main/java/com/payoya/diplomaproject/api/email/MailSenderService.java
@@ -3,21 +3,27 @@ package com.payoya.diplomaproject.api.email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
 
 @Component
 public class MailSenderService {
 
-    @Autowired
-     private JavaMailSender mailSender;
+    private JavaMailSender mailSender;
 
     public MailSenderService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
-
+    /*
+    Sending emails for users in Async. Created new thread for that purposes when this method is called
+     */
+    @Async
     public void sendEmail(String toEmail, String subject, String body){
         SimpleMailMessage message = new SimpleMailMessage();
+//        System.err.println("Time start : " + LocalDateTime.now());
         message.setFrom("noreplypayoyaEA@gmail.com");
         message.setTo(toEmail);
         message.setText(body);
@@ -26,6 +32,7 @@ public class MailSenderService {
         mailSender.send(message);
 
         System.err.println("email sent");
+//        System.err.println("Time end : " + LocalDateTime.now());
     }
 
 }
